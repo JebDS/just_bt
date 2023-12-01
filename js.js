3f4b38c241a517d6f67b8302472d7aa0e128e6a1
JebDS <jeb424ds@gmail.com>
2023-12-01 15:55:55 +0900

    틀

---
+++ js.js
@@ -0,0 +1,13 @@
+// const backgroundColorPicker = document.getElementById('tab1');
+// const borderPicker = document.getElementById('tab2');
+// const content = document.getElementById('BT');
+
+// backgroundColorPicker.addEventListener('input', update => {
+//     const selectedColor = backgroundColorPicker.value;
+//     content.style.backgroundColor = selectedColor;
+// });
+
+// borderPicker.addEventListener('input', () => {
+//     const selectedColor = borderPicker.value;
+//     content.style.borderBlockColorcolor = selectedColor;
+// });
